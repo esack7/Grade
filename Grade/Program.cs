9d5290b5f61775bbdba51137666c7640c4b6f9b9
Isaac Heist <isaac.heist@faithlife.com>
2018-03-15 08:35:55 -0700

    Finished Methods, Events, and Properties Section

--- Grade/Program.cs
+++ Grade/Program.cs
@@ -9,14 +9,39 @@ namespace Grade
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
-            book.AddGrade(91);
+
+			book.NameChanged += OnNameChanged;
+
+			book.Name = "Isaac's GradeBook";
+			book.Name = "GradeBook";
+			book.Name = "Homer's GradeBook";
+			book.Name = "Isaac's GradeBook";
+
+			book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine("Average grade: " + stats.AverageGrade);
-            Console.WriteLine("Highest grade: " + stats.HighestGrade);
-            Console.WriteLine("Lowest grade: " + stats.LowestGrade);
+			Console.WriteLine(book.Name);
+            WriteResult("Average grade: ", stats.AverageGrade);
+            WriteResult("Highest grade: ", (int)stats.HighestGrade);
+            WriteResult("Lowest grade: ", stats.LowestGrade);
+			Console.ReadLine();
         }
+
+		static void OnNameChanged(object sender, NameChangedEventArgs args)
+		{
+			Console.WriteLine($"GradeBook changing name from {args.ExistingName} to {args.NewName}");
+		}
+
+		static void WriteResult(string description, float result) 
+		{
+			Console.WriteLine($"{description}: {result:F2}");
+		}
+
+		static void WriteResult(string description, int result) 
+		{
+			Console.WriteLine($"{description}: {result:F2}");
+		}
     }
 }
