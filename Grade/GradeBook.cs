ede288127bee05b4601eab79a9b3e45801b903cc
Isaac Heist <isaac.heist@faithlife.com>
2018-03-16 08:44:46 -0700

    Got though control flow section.

--- Grade/GradeBook.cs
+++ Grade/GradeBook.cs
@@ -1,39 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Grade
 {
-    public class GradeBook
-    {
-        public GradeBook()
-        {
+	public class GradeBook
+	{
+		public GradeBook()
+		{
 			_name = "Empty";
-            grades = new List<float>();
-        }
+			grades = new List<float>();
+		}
 
-        public GradeStatistics ComputeStatistics()
-        {
-            GradeStatistics stats = new GradeStatistics();
+		public GradeStatistics ComputeStatistics()
+		{
+			GradeStatistics stats = new GradeStatistics();
 
-            float sum = 0;
-            foreach (float grade in grades)
-            {
-                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
-                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
-                sum += grade;
-            }
-            stats.AverageGrade = sum / grades.Count;
+			float sum = 0;
+			foreach (float grade in grades)
+			{
+				stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+				stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+				sum += grade;
+			}
+			stats.AverageGrade = sum / grades.Count;
 
 
-            return stats;
-        }
+			return stats;
+		}
 
-        public void AddGrade(float grade)
-        {
-            grades.Add(grade); 
-        }
+		internal void WriteGrades(TextWriter destination)
+		{
+			for (int i = 0; i < grades.Count; i++)
+			{
+				destination.WriteLine(grades[i]);
+			}
+		}
 
-        public string Name
+		public void AddGrade(float grade)
+		{
+			grades.Add(grade);
+		}
+
+		public string Name
 		{
 			get
 			{
@@ -42,23 +51,26 @@ namespace Grade
 
 			set
 			{
-				if (!String.IsNullOrEmpty(value))
+				if(string.IsNullOrEmpty(value))
 				{
-					if (_name != value)
-					{
-						NameChangedEventArgs args = new NameChangedEventArgs();
-						args.ExistingName = _name;
-						args.NewName = value;
-						
-						NameChanged(this, args);
-					}
-					_name = value;
+					throw new ArgumentException("Name cannot be null or empty");
 				}
+
+				if (_name != value)
+				{
+					NameChangedEventArgs args = new NameChangedEventArgs();
+					args.ExistingName = _name;
+					args.NewName = value;
+
+					NameChanged(this, args);
+				}
+				_name = value;
+
 			}
 		}
 		public event NameChangedDelegate NameChanged;
 
 		private string _name;
-        private List<float> grades; 
-    }
+		private List<float> grades;
+	}
 }
--- Grade/Program.cs
+++ Grade/Program.cs
@@ -1,37 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
 
 namespace Grade
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            GradeBook book = new GradeBook();
-
-			book.NameChanged += OnNameChanged;
-
-			book.Name = "Isaac's GradeBook";
-			book.Name = "GradeBook";
-			book.Name = "Homer's GradeBook";
-			book.Name = "Isaac's GradeBook";
-
-			book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-
-            GradeStatistics stats = book.ComputeStatistics();
-			Console.WriteLine(book.Name);
-            WriteResult("Average grade: ", stats.AverageGrade);
-            WriteResult("Highest grade: ", (int)stats.HighestGrade);
-            WriteResult("Lowest grade: ", stats.LowestGrade);
-			Console.ReadLine();
-        }
-
-		static void OnNameChanged(object sender, NameChangedEventArgs args)
 		{
-			Console.WriteLine($"GradeBook changing name from {args.ExistingName} to {args.NewName}");
+			GradeBook book = new GradeBook();
+
+			GetBookName(book);
+			AddGrades(book);
+			SaveGrades(book);
+			WriteResults(book);
+		}
+
+		private static void WriteResults(GradeBook book)
+		{
+			GradeStatistics stats = book.ComputeStatistics();
+			WriteResult("Average grade: ", stats.AverageGrade);
+			WriteResult("Highest grade: ", (int)stats.HighestGrade);
+			WriteResult("Lowest grade: ", stats.LowestGrade);
+			WriteResult(stats.Description, stats.LetterGrade);
+
+			Console.ReadLine();
+		}
+
+		private static void SaveGrades(GradeBook book)
+		{
+			using (StreamWriter outputFile = File.CreateText("grades.txt"))
+			{
+				book.WriteGrades(outputFile);
+			}
+		}
+
+		private static void GetBookName(GradeBook book)
+		{
+			try
+			{
+				Console.WriteLine("Enter a name: ");
+				book.Name = Console.ReadLine();
+			}
+			catch (ArgumentException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private static void AddGrades(GradeBook book)
+		{
+			book.AddGrade(91);
+			book.AddGrade(89.5f);
+			book.AddGrade(75);
+		}
+
+		static void WriteResult(string description, string result)
+		{
+			Console.WriteLine($"{description}: {result}");
 		}
 
 		static void WriteResult(string description, float result) 
